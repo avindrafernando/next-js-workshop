2036160115972b80ecf7b188e0284ada601524a1
adhithiravi <adhithiravi@gmail.com>
2024-06-25 13:56:59 -0500

    Add API urls to a const

--- app/conference/sessions/services/sessions.ts
+++ app/conference/sessions/services/sessions.ts
@@ -1,24 +1,18 @@
+import { SESSIONS_URL } from "@/lib/constants";
+
 export async function fetchSessions() {
   // Static Data Fetching
-  const sessions = await fetch(
-    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json"
-  );
+  const sessions = await fetch(SESSIONS_URL);
 
   // Static Data Fetching with Revalidation
-  //   const sessions = await fetch(
-  //     `https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json`,
-  //     {
-  //       next: { revalidate: 5 },
-  //     }
-  //   );
+  // const sessions = await fetch(SESSIONS_URL, {
+  //   next: { revalidate: 5 },
+  // });
 
   // Dynamic Data Fetching
-  //   const sessions = await fetch(
-  //     `https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json`,
-  //     {
-  //       cache: "no-cache",
-  //     }
-  //   );
+  // const sessions = await fetch(SESSIONS_URL, {
+  //   cache: "no-cache",
+  // });
 
   return sessions.json();
 }
